f02b803166ccf8c278fd84897870c072ff157169
Aahan341 <aaahan006@gmail.com>
2022-09-30 23:29:48 +0530

    day-4

---
+++ fruit.js
@@ -0,0 +1,218 @@
+
+let fruitsimage = [
+    {
+        image_url: "https://cdn.shopify.com/s/files/1/0449/5225/6667/products/Custard-Apple-A_540x.jpg?v=1632485463",
+        name: "Kimaye Custard Apple (Pack of 500 gm & 1 kg)",
+        price: "₹105 - ₹210",
+    },
+    {
+        image_url: "https://cdn.shopify.com/s/files/1/0449/5225/6667/products/cherry-imported-2A_360x.jpg?v=1629455322",
+        name:"Kimaye Cherries(Imported) (Pack of 125g)",
+        price: "₹419",
+    },
+    {
+        image_url: "https://cdn.shopify.com/s/files/1/0449/5225/6667/products/Pink-guava-cover_360x.jpg?v=1632551334",
+        name: "Kimaye Pink Guava (0.5kg - 1kg)",
+        price: "₹90 - ₹169",
+    },
+    {
+        image_url: "https://cdn.shopify.com/s/files/1/0449/5225/6667/products/Navratri-Combo-without-tags_360x.jpg?v=1633496774",
+        name: "Kimaye Navratri Combo",
+        price: "₹896",
+    },
+    {
+        image_url: "https://cdn.shopify.com/s/files/1/0449/5225/6667/products/grapefruit-cover_360x.jpg?v=1639461484",
+        name: "Kimaye Grapefruit (Pack of 2 & 4 pcs)",
+        price: "₹144 - ₹279",
+    },
+    {
+        image_url: "https://cdn.shopify.com/s/files/1/0449/5225/6667/products/Avocado_360x.jpg?v=1652191066",
+        name: "Kimaye Avocado (Imported) (1 pc )",
+        price: "285",
+    },
+    {
+        image_url: "https://cdn.shopify.com/s/files/1/0449/5225/6667/products/5e8b2ee6-ce8e-42e9-959f-d43c544f586a_360x.png?v=1616326949",
+        name: "Kimaye Blueberries (125g)",
+        price: "₹299",
+    },
+    {
+        image_url: "https://cdn.shopify.com/s/files/1/0449/5225/6667/products/dragon-fruit--white-cover_360x.jpg?v=1632470660",
+        name: "Kimaye Dragon Fruit (1 pc)",
+        price: "₹167",
+    },
+    {
+        image_url: "https://cdn.shopify.com/s/files/1/0449/5225/6667/products/Papaya-cover_360x.jpg?v=1638875170",
+        name: "Kimaye Papaya  (1 pc)",
+        price: "₹75",
+    },
+    {
+        image_url: "https://cdn.shopify.com/s/files/1/0449/5225/6667/products/Muskmelon-cover_360x.jpg?v=1633763200",
+        name: "Kimaye Muskmelon (1 pc)",
+        price: "₹83",
+    },
+    {
+        image_url: "https://cdn.shopify.com/s/files/1/0449/5225/6667/products/cover_19473cce-96ae-4149-aaa1-508f17ac6c28_360x.jpg?v=1648186636",
+        name: "Kimaye Watermeloan (1 pc)",
+        price: "₹89",
+    },
+    {
+        image_url: "https://cdn.shopify.com/s/files/1/0449/5225/6667/products/bd07b8e7-3643-4a98-996e-c85fd7d133a8_360x.png?v=1614765886",
+        name: "Red Globe Imported (400g)",
+        price: "₹195",
+    },
+    {
+        image_url: "https://cdn.shopify.com/s/files/1/0449/5225/6667/products/cover_360x.jpg?v=1647932788",
+        name: "Kimaye Custard Apple (Pack of 500 gm & 1 kg)",
+        price: "₹212 - ₹214",
+    },
+    {
+        image_url: "https://cdn.shopify.com/s/files/1/0449/5225/6667/products/a0cfceb5-76a4-4994-9d37-978c6dc3b309_360x.png?v=1611811512",
+        name: "Kimaye Custard Apple (Pack of 500 gm & 1 kg)",
+        price: "₹128 - ₹240",
+    },
+    {
+        image_url: "https://cdn.shopify.com/s/files/1/0449/5225/6667/products/Mini-Oranges-cover_bbce1a6a-4ab6-4a62-8928-3a956ed7b8cf_360x.jpg?v=1649913612",
+        name: "Kimaye Custard Apple (Pack of 500 gm & 1 kg)",
+        price: "₹119 - ₹210",
+    },
+    {
+        image_url: "https://cdn.shopify.com/s/files/1/0449/5225/6667/products/royal-gala-apple-cover_360x.jpg?v=1643012816",
+        name: "Kimaye Custard Apple (Pack of 500 gm & 1 kg)",
+        price: " ₹199",
+    },
+    {
+        image_url: "https://cdn.shopify.com/s/files/1/0449/5225/6667/products/Shimla-Apples-cover_360x.jpg?v=1632485361",
+        price: "₹105 - ₹210",
+    },
+    {
+        image_url: "https://cdn.shopify.com/s/files/1/0449/5225/6667/products/pear-imported_360x.jpg?v=1662621294",
+        name: "Kimaye Custard Apple (Pack of 500 gm & 1 kg)",
+        price: "₹105 - ₹210",
+    },
+    {
+        image_url: "https://cdn.shopify.com/s/files/1/0449/5225/6667/products/donut-peach-cover_360x.jpg?v=1647000498",
+        name: "Kimaye Custard Apple (Pack of 500 gm & 1 kg)",
+        price: "₹105 - ₹210",
+    },
+    {
+        image_url: "https://cdn.shopify.com/s/files/1/0449/5225/6667/products/Picture9_360x.jpg?v=1644394855",
+        name: "Kimaye Custard Apple (Pack of 500 gm & 1 kg)",
+        price: "₹105 - ₹210",
+    },
+    {
+        image_url: "https://cdn.shopify.com/s/files/1/0449/5225/6667/products/washington-applecover_360x.jpg?v=1641449752",
+        name: "Kimaye Custard Apple (Pack of 500 gm & 1 kg)",
+        price: "₹105 - ₹210",
+    },
+    {
+        image_url: "https://cdn.shopify.com/s/files/1/0449/5225/6667/products/Kinnaur-Apples-cover_360x.jpg?v=1634226790",
+        name: "Kimaye Custard Apple (Pack of 500 gm & 1 kg)",
+        price: "₹105 - ₹210",
+    },
+    {
+        image_url: "https://cdn.shopify.com/s/files/1/0449/5225/6667/products/1_6875ada8-f1e6-4589-b1fd-6f13a86b8ddb-min_360x.jpg?v=1614326164",
+        name: "Kimaye Custard Apple (Pack of 500 gm & 1 kg)",
+        price: "₹105 - ₹210",
+    },
+    {
+        image_url: "https://cdn.shopify.com/s/files/1/0449/5225/6667/products/golden-kiwi-cover_360x.jpg?v=1625147312",
+        price: "₹105 - ₹210",
+    },
+    {
+        image_url: "https://cdn.shopify.com/s/files/1/0449/5225/6667/products/221f2788-1a08-45ae-bdaf-7432d7ea0156_360x.png?v=1614326590",
+        name: "Kimaye Custard Apple (Pack of 500 gm & 1 kg)",
+        price: "₹105 - ₹210",
+    },
+    {
+        image_url: "https://cdn.shopify.com/s/files/1/0449/5225/6667/products/CopyofGuava_Main_Clean_360x.png?v=1606828311",
+        name: "Kimaye Custard Apple (Pack of 500 gm & 1 kg)",
+        price: "₹105 - ₹210",
+    },
+    {
+        image_url: "https://cdn.shopify.com/s/files/1/0449/5225/6667/products/1_3311b3de-1a38-4175-8e8c-ac9577ec85df-min_360x.jpg?v=1614324760",
+        name: "Kimaye Custard Apple (Pack of 500 gm & 1 kg)",
+        price: "₹105 - ₹210",
+    },
+    {
+        image_url: "https://cdn.shopify.com/s/files/1/0449/5225/6667/products/Yelakki-banana-cover_bc79dea2-08f8-404b-99f5-327a91213fbb_360x.jpg?v=1627026370",
+        name: "Kimaye Custard Apple (Pack of 500 gm & 1 kg)",
+        price: "₹105 - ₹210",
+    },
+    {
+        image_url: "https://cdn.shopify.com/s/files/1/0449/5225/6667/products/62cef687-de59-4f5a-acf3-4df1238c3e29-min_360x.png?v=1614324446",
+        name: "Kimaye Custard Apple (Pack of 500 gm & 1 kg)",
+        price: "₹105 - ₹210",
+    },
+    {
+        image_url: "https://cdn.shopify.com/s/files/1/0449/5225/6667/products/CopyofMosambi_Main_FixedShadows-min_360x.png?v=1614327057",
+        name: "Kimaye Custard Apple (Pack of 500 gm & 1 kg)",
+        price: "₹105 - ₹210",
+    },
+    {
+        image_url: "https://cdn.shopify.com/s/files/1/0449/5225/6667/products/47dfb7d1-d8d3-4c96-bda9-7dc85dfdec51_360x.png?v=1613364164",
+        name: "Kimaye Custard Apple (Pack of 500 gm & 1 kg)",
+        price: "₹105 - ₹210",
+    },
+    {
+        image_url: "https://cdn.shopify.com/s/files/1/0449/5225/6667/products/47dfb7d1-d8d3-4c96-bda9-7dc85dfdec51_119283b1-bac0-4798-8af7-e4259a3a4b22_360x.jpg?v=1633861455",
+        name: "Kimaye Custard Apple (Pack of 500 gm & 1 kg)",
+        price: "₹105 - ₹210",
+    },
+    {
+        image_url: "https://cdn.shopify.com/s/files/1/0449/5225/6667/products/24d0f9c9-fe83-40a3-84ab-300e73e4ff8d_360x.png?v=1611811511",
+        name: "Kimaye Custard Apple (Pack of 500 gm & 1 kg)",
+        price: "₹105 - ₹210",
+    },
+    {
+        image_url: "https://cdn.shopify.com/s/files/1/0449/5225/6667/products/0fce7fd6-f30e-45df-8c52-234acbd62023_360x.png?v=1611811509",
+        name: "Kimaye Custard Apple (Pack of 500 gm & 1 kg)",
+        price: "₹105 - ₹210",
+    },
+    {
+        image_url: "https://cdn.shopify.com/s/files/1/0449/5225/6667/products/Picture3_3cf8f5aa-4155-4a94-9768-9819bb41835f_360x.jpg?v=1641381097",
+        name: "Kimaye Custard Apple (Pack of 500 gm & 1 kg)",
+        price: "₹105 - ₹210",
+    },
+    {
+        image_url: "https://cdn.shopify.com/s/files/1/0449/5225/6667/products/Wholesome-combo_360x.jpg?v=1618497683",
+        name: "Kimaye Custard Apple (Pack of 500 gm & 1 kg)",
+        price: "₹105 - ₹210",
+    },
+    {
+        image_url: "https://cdn.shopify.com/s/files/1/0449/5225/6667/products/BlueberryKiwiCombo_360x.jpg?v=1662978491",
+        name: "Kimaye Custard Apple (Pack of 500 gm & 1 kg)",
+        price: "₹105 - ₹210",
+    },
+    {
+        image_url: "https://cdn.shopify.com/s/files/1/0449/5225/6667/products/2I8A7790_360x.jpg?v=1631346990",
+        name: "Kimaye Custard Apple (Pack of 500 gm & 1 kg)",
+        price: "₹105 - ₹210",
+    },
+    {
+        image_url: "https://cdn.shopify.com/s/files/1/0449/5225/6667/products/2I8A7821_360x.jpg?v=1631350656",
+        name: "Kimaye Custard Apple (Pack of 500 gm & 1 kg)",
+        price: "₹105 - ₹210",
+    },
+    {
+        image_url: "https://cdn.shopify.com/s/files/1/0449/5225/6667/products/2I8A7812_360x.jpg?v=1661146423",
+        name: "Kimaye Custard Apple (Pack of 500 gm & 1 kg)",
+        price: "₹105 - ₹210",
+    },
+]
+
+fruitsimage.forEach(function(el){
+    let card=document.createElement("div");
+
+    let image=document.createElement("img");
+    image.src=el.image_url;
+
+    let heading =document.createElement("h5")
+    heading.innerText=el.name;
+
+    let price=document.createElement("p");
+    price.innerText=el.price;
+
+
+    card.append(image,heading,price);
+    document.querySelector("#fruitsimage").append(card);
+})
